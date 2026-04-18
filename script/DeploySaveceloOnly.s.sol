278d7126b1d260e25b5507856063f0a905582df5
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2026-04-18 16:26:32 +0100

    DeploySavecelo

--- script/DeploySaveceloOnly.s.sol
+++ script/DeploySaveceloOnly.s.sol
@@ -4,6 +4,7 @@ pragma solidity ^0.8.20;
 import "forge-std/Script.sol";
 import {SaveCelo} from "../src/SaveCelo.sol";
 
+
 contract DeploySaveceloOnly is Script {
 
     address constant SAVE_TOKEN  = 0x69D847a5697dd61686F8ed51f2e886fE2103C350;
