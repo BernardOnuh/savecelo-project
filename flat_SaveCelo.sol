bdf6f19ad6c8419e7c1c8d5a6b1d9293fa56f7b3
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2026-04-20 21:29:20 +0100

    update pragma

--- flat_SaveCelo.sol
+++ flat_SaveCelo.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
+
 pragma solidity >=0.4.16 ^0.8.20;
 
 // lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol
-
 // OpenZeppelin Contracts (last updated v5.4.0) (token/ERC20/IERC20.sol)
 
 /**
