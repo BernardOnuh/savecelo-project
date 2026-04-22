fce88b584e56310c4a65035265556f38ebdc6921
Bernard.O <114490070+BernardOnuh@users.noreply.github.com>
2026-04-22 05:48:01 +0100

    update Deploy.sol

--- script/DeploySaveCelo.s.sol
+++ script/DeploySaveCelo.s.sol
@@ -17,7 +17,6 @@ contract DeploySaveCelo is Script {
         //    address cUSD = 0x874069Fa1Eb16D44d622F2e0Ca25eeA172369bC2;
         //
         //  Option C: Use your own deployed SaveToken address
-        
         //    address cUSD = <output from DeployToken script>;
         //
         address cUSD = vm.envAddress("CUSD_ADDRESS");
