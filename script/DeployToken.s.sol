fd194ae86b160da341f371e8d1af5936b38b9055
BernardOnuh <onuhbernard4@gmail.com>
2026-04-23 11:02:57 +0100

    token: SNGN (Save Naira) supply=10000000000

--- script/DeployToken.s.sol
+++ script/DeployToken.s.sol
@@ -12,10 +12,10 @@ contract DeployToken is Script {
         vm.startBroadcast(deployerKey);
 
         token = new SaveToken(
-            "Binance USD",   // name
-            "BUSD",          // symbol
+            "Save Naira",   // name
+            "SNGN",          // symbol
             18,              // decimals
-            2000000000        // initial supply: 1,000,000,000 SUSD minted to deployer
+            10000000000        // initial supply: 1,000,000,000 SUSD minted to deployer
         );
 
         vm.stopBroadcast();
