d2d1b6c53cc42ecb76e548283e84d235b80a32c8
BernardOnuh <onuhbernard4@gmail.com>
2026-04-27 20:55:28 +0100

    token: BUSD (Binance USD) supply=2000000000

--- script/DeployToken.s.sol
+++ script/DeployToken.s.sol
@@ -12,10 +12,10 @@ contract DeployToken is Script {
         vm.startBroadcast(deployerKey);
 
         token = new SaveToken(
-            "Save Pound",   // name
-            "SGBP",          // symbol
+            "Binance USD",   // name
+            "BUSD",          // symbol
             18,              // decimals
-            750000000        // initial supply: 1,000,000,000 SUSD minted to deployer
+            2000000000        // initial supply: 1,000,000,000 SUSD minted to deployer
         );
 
         vm.stopBroadcast();
